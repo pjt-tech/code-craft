f5cef7505e992461ce554e493999d7b9679126e2
tony-j-park <tony.j.park@selvas.com>
2024-03-18 16:57:42 +0900

    first init

---
+++ code-craft/src/main/java/com/example/codecraft/CodeCraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.codecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCraftApplication.class, args);
+	}
+
+}
---
+++ code-craft/src/test/java/com/example/codecraft/CodeCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
